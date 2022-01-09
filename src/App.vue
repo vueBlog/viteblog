0cb780fad49cf58c2c2d40e061b462b20406598c
fanyushun <fanyushun@zj.tech>
2022-01-09 15:24:50 +0800

    feat: add element-plus/axios/vueuse

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <el-button>Default</el-button>
 </template>
 
 <style>
